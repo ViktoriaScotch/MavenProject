9bbccf9da5efd1924b0a61052707cb6590ed9095
ViktoriaScotch <v.halimonova@gmail.com>
2019-09-04 11:27:57 +0300

    Add some checks and  hooks @Before and @After

--- src/test/java/ru/kinopoisk/TheAvengers.java
+++ src/test/java/ru/kinopoisk/TheAvengers.java
@@ -1,26 +1,47 @@
 package ru.kinopoisk;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 
 
 public class TheAvengers {
 
-    @Test
-    public void fistTest() throws InterruptedException {
+    public ChromeDriver driver;
+
+    @Before
+    public void login () throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", new File("").getAbsolutePath() + "\\src\\main\\resources\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
 
         driver.get("https://www.kinopoisk.ru/");
+        Assert.assertTrue(driver.getTitle().equals("КиноПоиск — Все фильмы планеты"));
 
         driver.findElementByXPath("//button[text()='Войти']").click();
+        Assert.assertTrue(driver.getTitle().equals("Авторизация"));
+
         driver.findElementByXPath("//input[@name='login']").sendKeys("c86539@urhen.com");
+        Assert.assertTrue(driver.findElementByXPath("//input[@name='login']").getAttribute("value").equals("c86539@urhen.com"));
         driver.findElementByXPath("//button[contains(@class, 'passp-form-button')]").click();
         Thread.sleep(1000);
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("passp-field-passwd")));
         driver.findElementByXPath("//*[@id='passp-field-passwd']").sendKeys("Qwerty123");
+        Assert.assertTrue(driver.findElementByXPath("//*[@id='passp-field-passwd']").getAttribute("value").equals("Qwerty123"));
         driver.findElementByXPath("//button[contains(@class, 'passp-form-button')]").click();
+    }
+
+    @Test
+    public void setMark() throws InterruptedException {
         Thread.sleep(1000);
         driver.findElementByXPath("//input[@name='kp_query']").sendKeys("Мстители: Война бесконечности");
         driver.findElementByXPath("//button[contains(@class, 'header-fresh-search')]").click();
@@ -28,6 +49,11 @@ public class TheAvengers {
         driver.findElementByXPath("//a[text()='Мстители: Война бесконечности']").click();
         Thread.sleep(1000);
         driver.findElementByXPath("//a[@class='s10']").click();
-   //     driver.quit();
+        Thread.sleep(1000);
+    }
+
+    @After
+    public void close () {
+        driver.quit();
     }
 }
